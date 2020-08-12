58c991f8347d2be2d29291ec4c9b6155fef1c0fd
aitiev <munarbek.aitiev@gmail.com>
2020-08-12 10:05:18 +0600

    Add serializer and posts view

--- main/views.py
+++ main/views.py
@@ -1,7 +1,22 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Post
+from .serializers import PostSerializer
+
 
 @api_view()
-def hello_world(request):
-    return Response('Hello World')
+def posts_list(request):
+    queryset = Post.objects.all()
+    serializer = PostSerializer(queryset, many=True, context={'request': request})
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+# TODO: FBV
+# TODO: CBV
+# TODO: CRUD
+# TODO: Viewsets
+# TODO: Pagination
+# TODO: Search
+# TODO: Filtering
+# TODO: Get author from request
